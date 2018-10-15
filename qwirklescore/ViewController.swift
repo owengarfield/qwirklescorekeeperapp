67b2610ade060d14040abf1670effb79b38c2111
Owen Richards <owengrichards@gmail.com>
2018-10-15 12:04:25 +0100

    Initial Commit

---
+++ qwirklescore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  qwirklescore
+//
+//  Created by Owen Richards on 15/10/2018.
+//  Copyright © 2018 Owen Garfield. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
