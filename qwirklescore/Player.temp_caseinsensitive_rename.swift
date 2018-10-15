329520089ecb037ff868f2b95016d8b01fd42665
Owen Richards <owengrichards@gmail.com>
2018-10-15 18:01:30 +0100

    uh?

---
+++ qwirklescore/Player.temp_caseinsensitive_rename.swift
@@ -0,0 +1,21 @@
+//
+//  player.swift
+//  qwirklescore
+//
+//  Created by Owen Richards on 15/10/2018.
+//  Copyright © 2018 Owen Garfield. All rights reserved.
+//
+
+import UIKit
+
+class Player: NSObject {
+
+    var name : String = ""
+    var turn : Int = 0
+    var points : Int = 0
+    var qwirkles : Int = 0
+    
+    override init() {
+    }
+    
+}
