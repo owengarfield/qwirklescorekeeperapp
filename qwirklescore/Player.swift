515b8f55405efa66c481b5149d91de81453db645
Owen Richards <owengrichards@gmail.com>
2018-10-15 17:23:10 +0100

    I hope this works...

---
+++ qwirklescore/Player.swift
@@ -0,0 +1,15 @@
+//
+//  player.swift
+//  qwirklescore
+//
+//  Created by Owen Richards on 15/10/2018.
+//  Copyright © 2018 Owen Garfield. All rights reserved.
+//
+
+
+class player: NSObject {
+
+    var name : String
+    var 
+    
+}
---
+++ qwirklescore/Score.swift
@@ -0,0 +1,25 @@
+//
+//  score.swift
+//  qwirklescore
+//
+//  Created by Owen Richards on 15/10/2018.
+//  Copyright © 2018 Owen Garfield. All rights reserved.
+//
+
+import UIKit
+
+class Score: NSObject {
+
+    var roundNumber: Int!
+    var playerIndex: Int!
+    var pointsScored: Int!
+    var qwirklesScored: Int!
+    
+    override init() {
+        }
+    
+}
+
+
+
+
